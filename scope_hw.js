ff5e6115ce2ade7f1c4e74f60ea656dd24f13334
Alan Hogarth <alan.hogarth@outlook.com>
2020-12-07 19:16:05 +0000

    initial commit

---
+++ scope_hw.js
@@ -0,0 +1,193 @@
+// Episode 1:
+
+// const scenario = {
+//     murderer: 'Miss Scarlet',
+//     room: 'Library',
+//     weapon: 'Rope'
+//   };
+  
+//   const declareMurderer = function() {
+//     return `The murderer is ${scenario.murderer}.`;
+//   }
+  
+//   const verdict = declareMurderer();
+//   console.log(verdict);
+
+//   prediction: the Murderer is "Miss Scarlet". The const scenario object can be accessed by the declareMurderer function and called as verdict.
+
+// Epsiode 2
+// const murderer = 'Professor Plum';
+
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
+
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// prediction: the murderer is Professor Plum: the const murderer variable cannot be altered inside the changeMurderer function. 
+
+// Episode 3
+// let murderer = 'Professor Plum';
+
+// const declareMurderer = function() {
+//   let murderer = 'Mrs. Peacock';
+//   return `The murderer is ${murderer}.`;
+// }
+
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
+
+// prediction: the first murderer is Mrs Peacock: the let variable inside the declareMurderer function is alterable. The second murderer is Professor Plum: the secondVerdict variable uses the original murderer variable. 
+
+// Episode 4
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
+
+// const declareAllSuspects = function() {
+//   let suspectThree = 'Colonel Mustard';
+//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
+
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
+
+// prediction: console.logging suspects will return Miss Scarlett, Professor Plum and Colonel Mustard; suspectThree is alterable inside the declareAllSuspects function because of 'let'. Console logging suspectThree outside of this function will return Mrs Peacock because she is Global and outside the local scope of the declareAllSuspects function.
+
+// Episode 5
+// const scenario = {
+//     murderer: 'Miss Scarlet',
+//     room: 'Kitchen',
+//     weapon: 'Candle Stick'
+//   };
+  
+//   const changeWeapon = function(newWeapon) {
+//     scenario.weapon = newWeapon;
+//   }
+  
+//   const declareWeapon = function() {
+//     return `The weapon is the ${scenario.weapon}.`;
+//   }
+  
+//   changeWeapon('Revolver');
+//   const verdict = declareWeapon();
+//   console.log(verdict);
+
+//   prediction: the weapon is the Revolver. The changeWeapon function can access the scenario object to change its properties, just as the declareWeapon function can access changeWeapon.
+
+// Episode 6
+// let murderer = 'Colonel Mustard';
+
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+
+//   const plotTwist = function() {
+//     murderer = 'Mrs. White';
+//   }
+
+//   plotTwist();
+// }
+
+// const declareMurderer = function () {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// prediction: Mrs White is the murderer. The original murderer variable is changeable, and changeMurderer and then plotTwist update the murderer. The changeMurderer call returns Mr Green, but the declareMurderer function call returns Mrs White. 
+
+// Episode 7
+// let murderer = 'Professor Plum';
+
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+
+//   const plotTwist = function() {
+//     let murderer = 'Colonel Mustard';
+
+//     const unexpectedOutcome = function() {
+//       murderer = 'Miss Scarlet';
+//     }
+
+//     unexpectedOutcome();
+//   }
+
+//   plotTwist();
+// }
+
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// prediction: the murderer is Mr Green. The changeMurderer function remains Mr Green because neither the plotTwist function (i.e let) or unexpectedOutcome function can override it.  
+
+
+// Episode 8
+// const scenario = {
+//     murderer: 'Mrs. Peacock',
+//     room: 'Conservatory',
+//     weapon: 'Lead Pipe'
+//   };
+  
+//   const changeScenario = function() {
+//     scenario.murderer = 'Mrs. Peacock';
+//     scenario.room = 'Dining Room';
+  
+//     const plotTwist = function(room) {
+//       if (scenario.room === room) {
+//         scenario.murderer = 'Colonel Mustard';
+//       }
+  
+//       const unexpectedOutcome = function(murderer) {
+//         if (scenario.murderer === murderer) {
+//           scenario.weapon = 'Candle Stick';
+//         }
+//       }
+  
+//       unexpectedOutcome('Colonel Mustard');
+//     }
+  
+//     plotTwist('Dining Room');
+//   }
+  
+//   const declareWeapon = function() {
+//     return `The weapon is ${scenario.weapon}.`
+//   }
+  
+//   changeScenario();
+//   const verdict = declareWeapon();
+//   console.log(verdict);
+
+//   prediction: the murderer is Colonel Mustard and weapon is candle-stick. changeScenario changes the room to dining room, and when plotTwist is called on dining room Col Mustard is identified as the murderer and then candle-stick as the weapon (through the unexpectedOutcome function)
+
+// Episode 9
+let murderer = 'Professor Plum';
+
+if (murderer === 'Professor Plum') {
+  let murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+// preditcion: the murderer is Professor Plum. The original let statement stands when declareMurderer is called. 
